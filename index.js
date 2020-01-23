6e69c0e454f9929569b5bbfa7ab6420913354c19
aya <aya.yoshida8@gmail.com>
2020-01-22 20:02:34 -0500

    first commit, package.json and index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+var inquirer = require("inquirer");
+var fs = require('fs');
+
+const questions = [
+
+]
+
+function writeToFile(fileName, data) {
+
+}
+
+function init() {
+
+}
